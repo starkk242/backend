2507c64c8938cd4b154ba29b4fa054c6249c7e76
Bimal <bimalsingh983@gmail.com>
2021-04-30 01:05:05 +0530

    Test commit

--- main.py
+++ main.py
@@ -6,10 +6,26 @@ app = Flask(__name__)
 def home():
     return "Hello, World!"
 
-@app.route("/send_data")
+@app.route("/send_data",,methods=['POST'])
 def send_data():
-	return "Hello Me"
+    first_name=request.form['fname']
+    fname_hash=hashlib.md5(first_name.encode()).hexdigest()
+
+    last_name=request.form['lname']
+    lname_hash=hashlib.md5(last_name.encode()).hexdigest()
+
+    email=request.form['email']
+    email_hash=hashlib.md5(email.encode()).hexdigest()
+
+    password=request.form['password']
+    paswd_hash = hashlib.md5(password.encode()).hexdigest()
+
+    gender=request.form['gender']
+    gender_hash=hashlib.md5(gender.encode()).hexdigest()
+
+    return "%s  %s  %s  %s  %s".format(fname_hash,lname_hash,email_hash,paswd_hash,gender_hash)
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80,debug=True)
-
