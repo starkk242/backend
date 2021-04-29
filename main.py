09d2e583551558e5b63992ef96b208eda493bea0
Bimal <bimalsingh983@gmail.com>
2021-04-30 01:13:35 +0530

    Error

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('landing.html')
+    return "Not Allowed"
 
 @app.route("/send_data",methods=['POST'])
 def send_data():
