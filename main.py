60ab71101997fd28546815ae7ddb037bee803263
Bimal <bimalsingh983@gmail.com>
2021-04-30 10:58:23 +0530

    Changes

--- main.py
+++ main.py
@@ -35,7 +35,27 @@ def send_data():
     mydb.commit()
 
     host=request.headers['Referer']
-    return redirect(host)
+    return redirect(host+"/login")
+
+@app.route('/auth',method=["POST"])
+def auth():
+    mydb=connect()
+    mycursor=mydb.cursor()
+
+    email=request.form['email']
+    email_hash=codecs.encode(email,'rot13')
+
+    password=request.form['password']
+    paswd_hash = hashlib.md5(password.encode()).hexdigest()
+
+    sql='SELECT password from customers where email = %s'
+    val=(email_hash,)
+    mycursor.execute(sql,val)
+    f=mycursor.fetchall()
+
+    if f[0][0]==paswd_hash:
+        return "Login Success"
+    return "Login Error"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80,debug=True)
